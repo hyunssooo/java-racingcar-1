4589645bc317ce2d76952964c5f3dddb8e0e4f9b
hyun <jhs695@naver.com>
2019-05-14 00:22:00 +0900

    fix 상수 추가

--- src/main/java/racingcargame/view/validation/InputViewValidation.java
+++ src/main/java/racingcargame/view/validation/InputViewValidation.java
@@ -2,8 +2,10 @@ package racingcargame.view.validation;
 
 public class InputViewValidation {
 
+    private static final int NEGATIVE_POINT = 0;
+
     public static void validNegativeNumber(int input) {
-        if (input < 0) {
+        if (input < NEGATIVE_POINT) {
             throw new IllegalArgumentException();
         }
     }
